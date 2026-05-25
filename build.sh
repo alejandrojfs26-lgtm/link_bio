1b5c4b914c20fe75aba42f026f30d317755d1c3f
Alejandro Fuentes <alejandrojfs26@gmail.com>
2026-05-25 21:33:00 +0200

    cleanup: remove duplicate script, restore build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source .venv/bin/activate
-pip install --upgrade pip
-pip install --break-system-packages -r requirements.txt
+pip install -r requirements.txt
 rm -rf public
 reflex init
 API_URL=https://linkbio-backend.up.railway.app/ reflex export --frontend-only
--- remote_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source .venv/bin/activate
-pip install --upgrade pip
-pip install --break-system-packages -r requirements.txt
-rm -rf public
-reflex init
-API_URL=https://linkbio-backend.up.railway.app/ reflex export --frontend-only
-unzip frontend.zip -d public
-rm -f frontend.zip
