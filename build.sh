511a4969781e2db56ad557189d43f85f9ff06b33
Alejandro Fuentes <alejandrojfs26@gmail.com>
2026-05-25 19:47:03 +0200

    backup before reorganization

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 pip install -r requirements.txt
 reflex init
-reflex export --frontend-only
+API_URL=https://linkbio-backend.up.railway.app/ reflex export --frontend-only
 rm -rf public
 unzip frontend.zip -d public
 rm -f frontend.zip
+desactivate
