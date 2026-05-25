fc711e7cca5f7635c14275c49ff7e9049625d98a
Alejandro Fuentes <alejandrojfs26@gmail.com>
2026-05-25 20:18:00 +0200

    fix build.sh: use uv, fix typos

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-pip install -r requirements.txt
-reflex init
-API_URL=https://linkbio-backend.up.railway.app/ reflex export --frontend-only
+uv pip install -r requirements.txt
+uv run reflex init
+API_URL=https://linkbio-backend.up.railway.app/ uv run reflex export --frontend-only
 rm -rf public
 unzip frontend.zip -d public
 rm -f frontend.zip
-desactivate
