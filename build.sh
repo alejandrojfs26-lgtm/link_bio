f60f80cd581c96ee7ef5142a1d4d373da0d02a03
Alejandro Fuentes <alejandrojfs26@gmail.com>
2026-05-25 20:58:05 +0200

    add public/ (frontend build) to repo for Vercel

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+source .venv/bin/activate
+pip install --upgrade pip
 pip install --break-system-packages -r requirements.txt
+rm -rf public
 reflex init
 API_URL=https://linkbio-backend.up.railway.app/ reflex export --frontend-only
-rm -rf public
 unzip frontend.zip -d public
 rm -f frontend.zip
