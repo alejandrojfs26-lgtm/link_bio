e9b9e4f7d19d21be897d4752ea625bd1310d6f23
Alejandro Fuentes <alejandrojfs26@gmail.com>
2026-05-25 20:42:20 +0200

    update project

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+source .venv/bin/activate
 uv pip install -r requirements.txt
 uv run reflex init
 API_URL=https://linkbio-backend.up.railway.app/ uv run reflex export --frontend-only
 rm -rf public
 unzip frontend.zip -d public
 rm -f frontend.zip
-desactivate
+deactivate
