297a402f421fafbcacc669daffa6df3d4306751d
Alejandro Fuentes <alejandrojfs26@gmail.com>
2026-05-25 21:19:51 +0200

    update project

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
+    cors_allowed_origins=["http://localhost:3000", "https://linkbio-frontend.vercel.app",],
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
