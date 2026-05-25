bc73bd4a53c074fce7301e06d7a2f23a1072cd53
Alejandro Fuentes <alejandrojfs26@gmail.com>
2026-05-25 22:47:58 +0200

    configure CORS and backend_host for Railway deployment

--- rxconfig.py
+++ rxconfig.py
@@ -4,9 +4,11 @@ config = rx.Config(
     app_name="link_bio",
     api_url="https://linkbio-backend.up.railway.app",
     deploy_url="https://linkbio-frontend.vercel.app",
+    backend_host="0.0.0.0",
     cors_allowed_origins=[
         "http://localhost:3000",
         "https://linkbio-frontend.vercel.app",
+        "https://linkbio-backend.up.railway.app",
     ],
     plugins=[
         rx.plugins.SitemapPlugin(),
